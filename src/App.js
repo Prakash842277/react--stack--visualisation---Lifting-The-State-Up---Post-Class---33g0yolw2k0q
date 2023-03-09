e6a318900ff8efc51dde34117e6e6ea89fa312a7
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-03-09 21:32:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,73 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
+import React from 'react';
 import Element from './components/element';
 
 function App() {
+  const [value, setValue] = React.useState('');
+  const [currpop, setPop] = React.useState('');
+  const [currtop, setTop] = React.useState('');
+  const [stack, setStack] = React.useState([]);
 
-  const [text, setText] = useState();
-  const [arr, setArr] = useState([]);
-  const [popEl, setPopEl] = useState('')
-  const [topEl, setTopEl] = useState('')
-
-  const handleInput = (e) => {
-    setText(e.target.value);
-  }
-
-  const handlePush = () => {
-    setArr([text,...arr]);
-  }
-
-  const handlePop = () => {
-    if (arr.length == 0) {
-      window.alert('Stack is empty')
-    }else{
-    setPopEl(arr.shift())
+  const hanldeChange = (event) => {
+    var curr = event.target.value;
+    setValue(curr);
+  };
+  const getTop = () => {
+    if (stack.length === 0) {
+      alert('Stack is empty');
+      return;
     }
-  }
-
-  const handleTop = () => {
-    if (arr.length == 0) {
-      window.alert('Stack is empty')
-    }else{
-    setTopEl(arr[0])
+    var top = stack[0];
+    setTop(top);
+  };
+  const push = () => {
+    if (value === '') {
+      alert('Please enter a value');
+      return;
     }
-  }
+    setStack([value, ...stack]);
+  };
+  const pop = () => {
+    if (stack.length === 0) {
+      alert('Stack is empty');
+      return;
+    }
+    var top = stack[0];
+    var arr = stack.slice();
+    arr.shift();
+    setStack(arr);
+    setPop(top);
+  };
 
   return (
     <div className="container">
       <h1 className="header">Stack</h1>
       <div className="row">
         <div className="col md-6 stack">
-          {arr.map((item,index)=>  <Element key={index} item={item} />)}
+          {stack.map((item, key) => (
+            <Element key={key} val={item} />
+          ))}
         </div>
         <div className="col md-3 operation">
           <h2>Operations</h2>
           <div className="input-group push">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handlePush}>
+            <button onClick={push} className="btn btn-outline-secondary" type="button" id="button-addon2">
               PUSH
             </button>
-            <input type="text" className="form-control" onChange={handleInput}/>
+            <input type="text" value={value} onChange={hanldeChange} className="form-control" />
           </div>
           <div className="input-group pop">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handlePop}>
+            <button onClick={pop} className="btn btn-outline-secondary" type="button" id="button-addon2">
               POP
             </button>
-            <input type="text" disabled className="form-control" value={popEl} />
+            <input type="text" disabled value={currpop} className="form-control" />
           </div>
           <div className="input-group top">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handleTop}>
+            <button onClick={getTop} className="btn btn-outline-secondary" type="button" id="button-addon2">
               TOP
             </button>
-            <input type="text" disabled className="form-control" value={topEl}/>
+            <input type="text" disabled value={currtop} className="form-control" />
           </div>
         </div>
       </div>
